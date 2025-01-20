60e442bf884f5801fe618bfa597cdbce5ce0c1a6
Ha Linh Nguyen <halinh150@gmail.com>
2025-01-20 21:49:16 +0700

    Update PayPalController.cs

--- EduTrailblaze/EduTrailblaze.API/Controllers/PayPalController.cs
+++ EduTrailblaze/EduTrailblaze.API/Controllers/PayPalController.cs
@@ -1,4 +1,5 @@
-﻿using EduTrailblaze.Services.Interfaces;
+﻿using EduTrailblaze.Services.DTOs;
+using EduTrailblaze.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EduTrailblaze.API.Controllers
@@ -14,32 +15,34 @@ namespace EduTrailblaze.API.Controllers
             _payPalService = payPalService;
         }
 
-        [HttpGet("payment-url")]
-        public IActionResult GetPaymentUrl(decimal amount, int orderId, int paymentId)
-        {
-            try
-            {
-                string paymentUrl = _payPalService.CreatePaymentUrl(amount, orderId, paymentId);
-                return Ok(paymentUrl);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //[HttpGet("payment-url")]
+        //public IActionResult GetPaymentUrl(decimal amount, int orderId, int paymentId)
+        //{
+        //    try
+        //    {
+        //        string paymentUrl = _payPalService.CreatePaymentUrl(amount, orderId, paymentId);
+        //        return Ok(paymentUrl);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
-        [HttpPost("validate")]
-        public IActionResult ValidatePaymentResponse(string paymentId, string payerId)
-        {
-            try
-            {
-                var payment = _payPalService.ExecutePayment(paymentId, payerId);
-                return Ok(payment);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //[HttpPost("validate")]
+        //public IActionResult ValidatePaymentResponse(string paymentId, string payerId)
+        //{
+        //    try
+        //    {
+               
+        //         var payment = _payPalService.ExecutePayment(paymentId, payerId);
+        //         Ok(payment);
+        //        Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
     }
 }
