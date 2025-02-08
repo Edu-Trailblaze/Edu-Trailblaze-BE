8fe47d17a34004d90670f124eed8325c4438fd8f
th3y3m <truongtanhuy3006@gmail.com>
2025-02-08 17:23:59 +0700

    Update CourseClassController.cs

--- EduTrailblaze/EduTrailblaze.API/Controllers/CourseClassController.cs
+++ EduTrailblaze/EduTrailblaze.API/Controllers/CourseClassController.cs
@@ -1,4 +1,5 @@
-﻿using EduTrailblaze.Services.Interfaces;
+﻿using EduTrailblaze.Services.DTOs;
+using EduTrailblaze.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EduTrailblaze.API.Controllers
@@ -8,5 +9,88 @@ namespace EduTrailblaze.API.Controllers
     public class CourseClassController : ControllerBase
     {
         private readonly ICourseClassService _courseClassService;
+
+        public CourseClassController(ICourseClassService courseClassService)
+        {
+            _courseClassService = courseClassService;
+        }
+
+        [HttpGet("{courseClassId}")]
+        public async Task<IActionResult> GetCourseClass(int courseClassId)
+        {
+            try
+            {
+                var courseClass = await _courseClassService.GetCourseClass(courseClassId);
+                if (courseClass == null)
+                {
+                    return NotFound();
+                }
+                return Ok(courseClass);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("newest/{courseId}")]
+        public async Task<IActionResult> GetNewestCourseClass(int courseId)
+        {
+            try
+            {
+                var courseClass = await _courseClassService.GetNewestCourseClass(courseId);
+                if (courseClass == null)
+                {
+                    return NotFound();
+                }
+                return Ok(courseClass);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCourseClasses()
+        {
+            try
+            {
+                var courseClasses = await _courseClassService.GetCourseClasses();
+                return Ok(courseClasses);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCourseClass([FromBody] CreateCourseClassRequest courseClass)
+        {
+            try
+            {
+                await _courseClassService.AddCourseClass(courseClass);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCourseClass([FromBody] UpdateCourseClassRequest courseClass)
+        {
+            try
+            {
+                await _courseClassService.UpdateCourseClass(courseClass);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
