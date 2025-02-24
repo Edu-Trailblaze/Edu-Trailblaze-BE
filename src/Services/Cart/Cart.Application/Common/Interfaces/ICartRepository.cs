6d230931ef7d79a274480ddf89388c1da9d805ad
Ha Linh Nguyen <halinh150@gmail.com>
2025-02-24 18:50:52 +0700

    add ICartRepo

---
+++ src/Services/Cart/Cart.Application/Common/Interfaces/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Application.Common.Interfaces
+{
+    public interface ICartRepository
+    {
+        Task<Domain.Entities.Cart?> GetBasketByUsername(string username);
+        Task<Domain.Entities.Cart> UpdateBasket(Domain.Entities.Cart basket, DistributedCacheEntryOptions entryOptions);
+        Task<bool> DeleteBasketByUsername(string username);
+    }
+}
