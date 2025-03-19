efe9b6527ac0b7d41fff32dc035e8bf1b1e1f133
th3y3m <truongtanhuy3006@gmail.com>
2025-03-19 22:58:11 +0700

    Update NotificationHub.cs

--- EduTrailblaze/EduTrailblaze.Services/Hubs/NotificationHub.cs
+++ EduTrailblaze/EduTrailblaze.Services/Hubs/NotificationHub.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace EduTrailblaze.Services.Hubs
 {
+    [Authorize]
     public class NotificationHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"User connected: {Context.UserIdentifier} (Connection ID: {Context.ConnectionId})");
+            await base.OnConnectedAsync();
+        }
     }
 }
